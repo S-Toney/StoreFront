71c8dbac3db89144516e374115f17eb975719728
Stacy Toney <stacy.toney@protonmail.com>
2020-12-28 23:28:50 -0600

    js alert greeting added

--- StoreFront.UI/Scripts/customJS.js
+++ StoreFront.UI/Scripts/customJS.js
@@ -1,6 +1,21 @@
 ﻿//alert("Hello World");
 
-var i = 0;
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening';
+} else if (hourNow > 12){
+    greeting = "Good afternoon";
+} else if (hourNow > 0) {
+    greeting = "Good morning";
+} else {
+    greeting = "Welcome";
+}
+alert(greeting);
+
+var i = 1;
 var txt = 'Lorem ipsum typing effect!'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
