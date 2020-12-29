36875d6056730c7fce9d13d09a074659ccf0568a
Stacy Toney <stacy.toney@protonmail.com>
2020-12-28 22:48:29 -0600

    added custom js and jQuery pages and typeWriter function

---
+++ StoreFront.UI/Scripts/customJS.js
@@ -0,0 +1,13 @@
+﻿//alert("Hello World");
+
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("404-1").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
---
+++ StoreFront.UI/Scripts/customjQuery.js
@@ -0,0 +1,9 @@
+﻿///  <reference path="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" />
+//  XML reference gives us intellisense for jQuery
+
+//  Below is a fully-qualified ready function
+//  It waits for HTML and CSS to load before executing the code inside of the function
+
+//jQuery(document).ready(function () {
+//    console.log("Hello World jQuery");
+//});
